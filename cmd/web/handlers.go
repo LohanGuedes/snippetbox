dfbecf244a8a28c4075af085ae0f22fdfcd478b6
Lguedes <lohan.guedes@nutrix.tech>
2023-11-21 06:27:56 -0300

    Dependency Injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
+func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -22,19 +21,19 @@ func Home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Println(err.Error())
+		app.errorLog.Println(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func SnippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) SnippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -43,12 +42,12 @@ func SnippetView(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "display a specific snipped with the ID %d...", id)
 }
 
-func SnippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) SnippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
-	w.Write([]byte("Create snippet"))
+	w.Write([]byte("Create snippet..."))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,23 +4,43 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse() // Automatically quits if there's an error.
 
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 	mux := http.NewServeMux()
 
+	// Serve a directory
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", Home)
-	mux.HandleFunc("/snippet/view", SnippetView)
-	mux.HandleFunc("/snippet/create", SnippetCreate)
+	mux.HandleFunc("/", app.Home)
+	mux.HandleFunc("/snippet/view", app.SnippetView)
+	mux.HandleFunc("/snippet/create", app.SnippetCreate)
 
-	log.Println("Starting server on port", *addr)
+	srv := &http.Server{
+		Addr:     "127.0.0.1" + *addr, //Addr needs 127.0.0.1 to silence MacOS popUp
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
-	err := http.ListenAndServe("127.0.0.1"+*addr, mux) // The port is to be declared like this in MacOS in order to remove that allow popup
-	log.Fatal(err)
+	infoLog.Println("Starting server on port", *addr)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
