82dec5982d33c96f4b6fc9def9023d1e99f6fef0
Lguedes <--global>
2023-12-23 20:47:37 -0300

    Replace PermittedInt to PermittedValue [GENERIC IMPLEMENTATION!]
    This app would work well without the generic implementation for this funcion.
    
    This is an example that was added for soly pourpose of LEARNING.

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -58,6 +58,15 @@ func PermittedInt(value int, permittedValues ...int) bool {
 	return false
 }
 
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
+
 func Matches(value string, rx *regexp.Regexp) bool {
 	return rx.MatchString(value)
 }
